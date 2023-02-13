6853439d05d03602d85e79f94b640455700e59b7
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-02-13 13:07:39 +0300

    Assignment partially completed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
-
+class CashRegister
+    attr_accessor :total
+  
+    def initialize(discount = nil)
+      @discount = discount
+      @total = 0
+      @items = []
+    end
+  
+    def add_item(title, price, quantity = 1)
+      @total += price * quantity
+      quantity.times { @items << title }
+    end
+  
+    def apply_discount
+      return "There is no discount to apply." unless @discount
+  
+      @total *= (100 - @discount) / 100.0
+      "After the discount, the total comes to $#{@total.round(2)}."
+    end
+  
+    def items
+      @items
+    end
+  
+    def void_last_transaction
+      @total -= @items.pop.size * @total / @items.size
+    end
+  end
+  
