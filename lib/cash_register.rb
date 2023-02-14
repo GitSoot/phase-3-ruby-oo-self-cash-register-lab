2fa8119b144b2db6f781e47f95de1848febfb339
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-02-14 09:29:41 +0300

    commit tree 885dc8b9ac8d9dacca0b2fcedac92ae3b3afcd39

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,30 +1,29 @@
 class CashRegister
-    attr_accessor :total
+  attr_accessor :total, :discount, :items, :last_transaction_amount
   
-    def initialize(discount = nil)
-      @discount = discount
-      @total = 0
-      @items = []
-    end
-  
-    def add_item(title, price, quantity = 1)
-      @total += price * quantity
-      quantity.times { @items << title }
-    end
-  
-    def apply_discount
-      return "There is no discount to apply." unless @discount
-  
-      @total *= (100 - @discount) / 100.0
-      "After the discount, the total comes to $#{@total.round(2)}."
-    end
-  
-    def items
-      @items
-    end
-  
-    def void_last_transaction
-      @total -= @items.pop.size * @total / @items.size
-    end
+  def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @items = []
   end
   
+  def add_item(title, price, quantity = 1)
+  self.total += price * quantity
+  quantity.times { items << title }
+  self.last_transaction_amount = price * quantity
+  end
+  
+  def apply_discount
+  if discount > 0
+  self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+  "After the discount, the total comes to $#{self.total}."
+  else
+  "There is no discount to apply."
+  end
+  end
+  
+  def void_last_transaction
+  self.total = self.total - self.last_transaction_amount
+  end
+  
+  end
